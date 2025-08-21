57217274a31aa19d799ab74f6aeb8d10de435a30
Dominik Havelka <havelkadomini@gmail.com>
2025-08-21 12:19:05 +0200

    refactor: Move parameter classes into a separate file

---
+++ TspAcoSolver/Parameters.cs
@@ -0,0 +1,49 @@
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace TspAcoSolver
+{
+    public class Config
+    {
+        public SolvingParams Read(string path)
+        {
+            using System.IO.StreamReader r = new(@"..\..\..\..\data\default_config.yaml");
+            IDeserializer deserializer = new DeserializerBuilder()
+                .WithNamingConvention(UnderscoredNamingConvention.Instance)  // see height_in_inches in sample yml
+                .Build();
+
+            SolvingParams p = deserializer.Deserialize<SolvingParams>(r.ReadToEnd());
+            return p;
+        }
+    }
+
+    public class PheromoneParams
+    {
+        public double EvaporationCoef { get; set; }
+        public double DecayCoef { get; set; }
+        public double InitialPheromoneAmount { get; set; }
+        public double PheromoneAmount { get; set; }
+    }
+    public class ColonyParams
+    {
+        public int AntCount { get; set; }
+        public int ThreadCount { get; set; }
+        public double TrailLevelFactor { get; set; }
+        public double AttractivenessFactor { get; set; }
+        public double ExploProportionConst { get; set; }
+    }
+    public class TerminationParams
+    {
+        public string TerminationRule { get; set; }
+        public int IterationCount { get; set; }
+        public double CeilingPercentage { get; set; }
+        public int InRowTerminationCount { get; set; }
+    }
+
+    public class SolvingParams
+    {
+        public PheromoneParams PheromoneParams { get; set; }
+        public TerminationParams TerminationParams { get; set; }
+        public ColonyParams ColonyParams { get; set; }
+    }
+}
